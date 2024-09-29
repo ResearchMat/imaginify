e199a3c2968f0671cf6c7f30796a3ed26ed6ee45
Matthew Dsouza <matthew.srccode@gmail.com>
2024-09-29 12:01:33 +0530

    changed the middleware to clerkmiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,6 @@
-// import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-// export default clerkMiddleware()
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// }
-import { authMiddleware } from '@clerk/nextjs/server'
-
-export default authMiddleware({
-  ignoredRoutes: ['/api/webhooks(.*)'],
-})
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
@@ -24,6 +10,7 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 }
+
 
 // import { authMiddleware } from "@clerk/nextjs/server";
  
